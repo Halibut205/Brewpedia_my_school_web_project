fb18769d7045533cc093a88ba7ac0c4f7c0684ff
Halibut205 <ducpm.23810320030@epu.edu.vn>
2025-04-04 11:22:31 +0700

    sửa js header

--- assets/scripts/linkHeader.js
+++ assets/scripts/linkHeader.js
@@ -8,3 +8,13 @@ fetch("../views/templates/header.html") // ?? thêm s làm gì ?
         document.head.appendChild(link);
     })
     .catch((error) => console.error("Lỗi load header:", error));
+    
+    
+    window.addEventListener('scroll', function() {
+        const header = document.querySelector('.header');
+        if (window.scrollY > 53) {
+            header.classList.add('sticky');
+        } else {
+            header.classList.remove('sticky');
+        }
+    });
